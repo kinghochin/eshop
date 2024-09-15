e45c72222fb8c4baebf2dbcecd7f7406bac01b72
Chris Chin <chris.chin@brandnudge.co.uk>
2024-09-15 23:26:59 +0100

    Section 0: preparation done and get ready for project start

---
+++ components/section/ShoppingCartButton.tsx
@@ -0,0 +1,10 @@
+import { ShoppingBagIcon } from 'lucide-react'
+import React from 'react'
+
+const ShoppingCartButton = () => {
+  return (
+    <div><ShoppingBagIcon cursor={"pointer"} size={26}/></div>
+  )
+}
+
+export default ShoppingCartButton
