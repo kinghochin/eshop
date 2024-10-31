4b20cdede5045f80c3abf1ab3dbfd5e11a8ee7d2
Chris Chin <88380016+kinghochin@users.noreply.github.com>
2024-10-31 22:14:02 +0000

    Update page.tsx

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,16 @@
 import { SignUp } from '@clerk/nextjs'
 import React from 'react'
 
+export async function generateStaticParams() {
+  // If there are no specific dynamic parameters, return an array with a default value
+  return [
+    { 'sign-up': [] }, // This would correspond to the base route /sign-up
+    // If you have specific parameters, you can define them like this:
+    // { 'sign-up': ['example1'] },
+    // { 'sign-up': ['example2'] },
+  ];
+}
+
 const signUpPage = () => {
   return (
     <div className='flex items-center justify-center h-screen w-full'>
