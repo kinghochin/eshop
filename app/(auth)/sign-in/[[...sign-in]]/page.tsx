a256426de561a61155c8546e770fe1a654fc527e
Chris Chin <88380016+kinghochin@users.noreply.github.com>
2024-11-01 00:49:52 +0000

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,6 @@
 import { SignIn } from '@clerk/nextjs'
 import React from 'react'
 
-export async function generateStaticParams() {
-  return [
-    { signIn: [] }, // This would correspond to the base route /sign-in
-  ];
-}
-
 const SignInPage = () => {
   return (
     <div className='flex items-center justify-center h-screen w-full'>
